3a7572cf6525fb0451675cbff84d9f496364492d
baphuoc231990 <baphuoc231990@1a24b0a5-4aa8-793c-8372-a6efbf75f8d7>
2013-04-20 08:20:06 +0000

    create project & configures it to work with ZXing library

---
+++ QRCode/QRCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QRCode
+//
+//  Created by Nguyen Ba Phuoc on 4/20/13.
+//  Copyright (c) 2013 Nguyen Ba Phuoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ QRCode/QRCode/scr/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by Nguyen Ba Phuoc on 4/20/13.
+//  Copyright (c) 2013 Nguyen Ba Phuoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
