bb5d27a18014f51f6442220e8d1ec8bb5d7e8f18
baphuoc231990 <baphuoc231990@1a24b0a5-4aa8-793c-8372-a6efbf75f8d7>
2013-04-20 08:20:06 +0000

    create project & configures it to work with ZXing library

---
+++ trunk/QRCode/QRCode/scr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by Nguyen Ba Phuoc on 4/20/13.
+//  Copyright (c) 2013 Nguyen Ba Phuoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QRCodeReader.h"
+#import "ZXingWidgetController.h"
+
+@interface ViewController : UIViewController
+
+@end
