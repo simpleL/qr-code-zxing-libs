48e8531aae41f9839edd9500582563c207e6ce28
baphuoc231990 <baphuoc231990@1a24b0a5-4aa8-793c-8372-a6efbf75f8d7>
2013-05-03 06:11:52 +0000

    add camera recorder

---
+++ QRCode/QRCode/scr/ScanView.h
@@ -0,0 +1,13 @@
+//
+//  ScanView.h
+//  QRCode
+//
+//  Created by Nguyen Ba Phuoc on 5/3/13.
+//  Copyright (c) 2013 Nguyen Ba Phuoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScanView : UIView
+
+@end
