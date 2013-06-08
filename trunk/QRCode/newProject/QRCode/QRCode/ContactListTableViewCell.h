ba19c8decf6e20f16ee6a28d9779e680143be902
baphuoc231990 <baphuoc231990@1a24b0a5-4aa8-793c-8372-a6efbf75f8d7>
2013-06-08 13:04:16 +0000

    add new project, using storyboard to design a new interface

---
+++ trunk/QRCode/newProject/QRCode/QRCode/ContactListTableViewCell.h
@@ -0,0 +1,27 @@
+//
+//  ContactListTableViewCell.h
+//  QRCode
+//
+//  Created by Nguyen Ba Phuoc on 08/06/2013.
+//  Copyright (c) 2013 Nguyen Ba Phuoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactListTableViewCell : UITableViewCell
+{
+    IBOutlet UILabel        *_lbFullName;
+    IBOutlet UILabel        *_lbPhoneNumber;
+    IBOutlet UILabel        *_lbEmail;
+    IBOutlet UILabel        *_lbPersonalSite;
+    IBOutlet UITextView     *_tvAddress;
+    
+    IBOutlet UIButton       *_btnCall;
+    IBOutlet UIButton       *_btnGoToSite;
+    IBOutlet UIButton       *_btnMailTo;
+}
+
+-(void) setInfoWithFullName:(NSString*)fullName phoneNumber:(NSString*)phoneNumber email:(NSString*)email personalSite:(NSString*)personalSite address:(NSString*)address;
+-(IBAction)btnClicked:(id)sender;
+
+@end
---
+++ trunk/QRCode/newProject/QRCode/QRCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QRCode
+//
+//  Created by Nguyen Ba Phuoc on 08/06/2013.
+//  Copyright (c) 2013 Nguyen Ba Phuoc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
